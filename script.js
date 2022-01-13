3a1439fb89082af97b48c7d6e70ae3e9686bc0ce
Jayme Borges <jayme.moia@gmail.com>
2022-01-13 00:13:12 -0300

    add jquery

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("div.select").hover(function(){
-      $(this).css('border', '1px solid #c93d30');
-      }, function(){
-      $(this).css('border', '1px solid #f9f9f9');
-    });
-  });
